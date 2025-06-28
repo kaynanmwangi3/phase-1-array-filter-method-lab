04f1385d9b08c415af55ad75c64eb957101d98f6
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-06-28 09:26:04 +0300

    updates files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// Function that takes an array of drivers' names and a string, returns matching list of drivers (case insensitive)
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+// Function that takes an array of drivers' names and a string, returns all drivers whose names begin with provided letters
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+// Function that takes an array of driver objects and a string, returns each element whose name property matches the provided string
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+}
